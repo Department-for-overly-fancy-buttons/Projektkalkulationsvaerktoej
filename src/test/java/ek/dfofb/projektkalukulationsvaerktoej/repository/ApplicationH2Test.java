837d68a87ac803e209f8e916d116dbf105813ff2
Frede-72 <frjo0005@stud.kea.dk>
2025-11-25 11:48:43 +0100

    ADDED: Test application for h2

---
+++ src/test/java/ek/dfofb/projektkalukulationsvaerktoej/repository/ApplicationH2Test.java
@@ -0,0 +1,13 @@
+package ek.dfofb.projektkalukulationsvaerktoej.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationH2Test {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
