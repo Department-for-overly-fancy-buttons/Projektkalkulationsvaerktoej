1dd3071ca96474d60155d1f24c28ff8f8c365a29
Frede-72 <frjo0005@stud.kea.dk>
2025-11-25 11:51:49 +0100

    CHANGED: list to set + correct capitization

--- src/main/java/ek/dfofb/projektkalukulationsvaerktoej/repository/interfaces/ITaskRepository.java
+++ src/main/java/ek/dfofb/projektkalukulationsvaerktoej/repository/interfaces/ITaskRepository.java
@@ -4,17 +4,18 @@ import ek.dfofb.projektkalukulationsvaerktoej.model.Account;
 import ek.dfofb.projektkalukulationsvaerktoej.model.Task;
 
 import java.util.List;
+import java.util.Set;
 
 public interface ITaskRepository {
 
-    Task getTaskByID(int TaskID);
+    Task getTaskByID(int taskID);
 
-    List<Task> getAllTasksForProjects(int projectID);
+    Set<Task> getAllTasksForProjects(int projectID);
 
-    List<Task> getAllSubTasks(int TaskID);
+    List<Task> getAllSubTasks(int taskID);
 
     List<Task> getAllTasksForAccount(int accountID);
 
-    List<Account> getAllAssignedToProject(int ProjectID);
+    List<Account> getAllAssignedToProject(int projectID);
 
 }
