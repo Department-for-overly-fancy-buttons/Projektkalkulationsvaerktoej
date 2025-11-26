964b63188b1fa9a7f6086cf72ac80a68265f9653
Frede-72 <frjo0005@stud.kea.dk>
2025-11-26 09:25:32 +0100

    ADDED: add and update tasks throws DataAccessException

--- src/main/java/ek/dfofb/projektkalukulationsvaerktoej/repository/TaskRepository.java
+++ src/main/java/ek/dfofb/projektkalukulationsvaerktoej/repository/TaskRepository.java
@@ -3,6 +3,7 @@ package ek.dfofb.projektkalukulationsvaerktoej.repository;
 import ek.dfofb.projektkalukulationsvaerktoej.model.Account;
 import ek.dfofb.projektkalukulationsvaerktoej.model.Task;
 import ek.dfofb.projektkalukulationsvaerktoej.repository.interfaces.ITaskRepository;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -20,45 +21,68 @@ public class TaskRepository implements ITaskRepository {
     }
 
     @Override
-    public Task getTaskByID(int taskID) {
+    public Task getTaskByID(int taskID) throws DataAccessException{
         String sql = "SELECT * FROM Tasks where TaskID = ?";
-        List<Task> task =jdbcTemplate.query(sql,new TaskRowMapper(),taskID);
-        return task.get(0);
+        Task task = jdbcTemplate.queryForObject(sql, new TaskRowMapper(), taskID);
+        return task;
     }
 
     @Override
-    public Set<Task> getAllTasksForProjects(int projectID) {
+    public Set<Task> getAllTasksForProjects(int projectID) throws DataAccessException{
         String sql = "SELECT * FROM Tasks where ProjectID = ?";
-        List<Task> tasks = jdbcTemplate.query(sql,new TaskRowMapper(),projectID);
+        List<Task> tasks = jdbcTemplate.query(sql, new TaskRowMapper(), projectID);
         Set<Task> taskSet = new HashSet<>();
-        for(Task task: tasks){
+        for (Task task : tasks) {
             taskSet.add(task);
         }
         return taskSet;
     }
 
     @Override
-    public Set<Task> getAllSubTasks(int taskID) {
+    public Set<Task> getAllSubTasks(int taskID) throws DataAccessException{
+        String sql = "SELECT * FROM Tasks where ParentID = ?";
+        List<Task> tasks = jdbcTemplate.query(sql, new TaskRowMapper(), taskID);
+        Set<Task> taskSet = new HashSet<>();
+        for (Task task : tasks) {
+            taskSet.add(task);
+        }
+        return taskSet;
+    }
+
+    @Override
+    public Set<Task> getAllTasksForAccount(int accountID) throws DataAccessException {
         return Set.of();
     }
 
     @Override
-    public Set<Task> getAllTasksForAccount(int accountID) {
+    public Set<Account> getAllAssignedToTask(int taskID) throws DataAccessException{
         return Set.of();
     }
 
     @Override
-    public Set<Account> getAllAssignedToProject(int projectID) {
-        return Set.of();
-    }
-
-    @Override
-    public boolean addTask(Task task) {
+    public boolean addTask(Task task) throws DataAccessException {
+        String sql = "INSERT INTO Tasks (Name, Description, HourEstimate, Completed," +
+                "  Deadline, StartDate, ProjectID, ParentID) VALUES (?,?,?,?,?,?,?,?)";
+        int rowsInserted = jdbcTemplate.update(sql, task.getName(), task.getDescription(), task.getHourEstimate(),
+                task.isCompleted(), task.getDeadLine(), task.getStartDate(),
+                task.getProjectID(), task.getParentID());
+        if (rowsInserted == 1) {
+            return true;
+        }
         return false;
     }
 
     @Override
-    public Task updateTask(Task task) {
-        return null;
+    public Task updateTask(Task task) throws DataAccessException{
+        String sql = "UPDATE tasks SET Name=?, Description=?, HourEstimate=?, Completed=?, " +
+                "Deadline=?, StartDate=? WHERE TaskID = ?";
+        jdbcTemplate.update(sql,task.getName(),task.getDescription(),task.getHourEstimate(),
+                task.isCompleted(),task.getDeadLine(),task.getStartDate(),task.getTaskID());
+        return task;
+    }
+
+    @Override
+    public boolean deleteTask(int taskID) {
+        return false;
     }
 }
