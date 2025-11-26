74611b2451ce903b4b139b6be21804a17d909ee1
Frede-72 <frjo0005@stud.kea.dk>
2025-11-26 10:24:02 +0100

    CHANGED: simplified/removed unnecessary code

--- src/main/java/ek/dfofb/projektkalukulationsvaerktoej/repository/TaskRepository.java
+++ src/main/java/ek/dfofb/projektkalukulationsvaerktoej/repository/TaskRepository.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Repository;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 
 @Repository
 public class TaskRepository implements ITaskRepository {
@@ -21,20 +20,19 @@ public class TaskRepository implements ITaskRepository {
     }
 
     @Override
-    public Task getTaskByID(int taskID) throws DataAccessException{
+    public Task getTaskByID(int taskID) throws DataAccessException {
         String sql = "SELECT * FROM Tasks where TaskID = ?";
-        Task task = jdbcTemplate.queryForObject(sql, new TaskRowMapper(), taskID);
-        return task;
+        return jdbcTemplate.queryForObject(sql, new TaskRowMapper(), taskID);
     }
 
     @Override
-    public Set<Task> getAllTasksForProjects(int projectID) throws DataAccessException{
+    public Set<Task> getAllTasksForProjects(int projectID) throws DataAccessException {
         String sql = "SELECT * FROM Tasks where ProjectID = ?";
         return getTasks(projectID, sql);
     }
 
     @Override
-    public Set<Task> getAllSubTasks(int taskID) throws DataAccessException{
+    public Set<Task> getAllSubTasks(int taskID) throws DataAccessException {
         String sql = "SELECT * FROM Tasks where ParentID = ?";
         return getTasks(taskID, sql);
     }
@@ -46,17 +44,20 @@ public class TaskRepository implements ITaskRepository {
     }
 
     private Set<Task> getTasks(int accountID, String sql) {
-        List<Task> tasks = jdbcTemplate.query(sql,new TaskRowMapper(),accountID);
-        Set<Task> taskSet = new HashSet<>();
-        for (Task task : tasks) {
-            taskSet.add(task);
-        }
-        return taskSet;
+        List<Task> tasks = jdbcTemplate.query(sql, new TaskRowMapper(), accountID);
+        return new HashSet<>(tasks);
     }
 
     @Override
-    public Set<Account> getAllAssignedToTask(int taskID) throws DataAccessException{
+    public Set<Account> getAllAssignedToTask(int taskID) throws DataAccessException {
         return Set.of();
+    }
+
+    @Override
+    public boolean assignAccountToTask(int accountID, int taskID) {
+        String sql = "INSERT INTO taskList (AccountID,TaskID) VALUES(?,?)";
+        int rowsInserted = jdbcTemplate.update(sql, accountID, taskID);
+        return rowsInserted == 1;
     }
 
     @Override
@@ -66,28 +67,22 @@ public class TaskRepository implements ITaskRepository {
         int rowsInserted = jdbcTemplate.update(sql, task.getName(), task.getDescription(), task.getHourEstimate(),
                 task.isCompleted(), task.getDeadLine(), task.getStartDate(),
                 task.getProjectID(), task.getParentID());
-        if (rowsInserted == 1) {
-            return true;
-        }
-        return false;
+        return rowsInserted == 1;
     }
 
     @Override
-    public Task updateTask(Task task) throws DataAccessException{
+    public Task updateTask(Task task) throws DataAccessException {
         String sql = "UPDATE tasks SET Name=?, Description=?, HourEstimate=?, Completed=?, " +
                 "Deadline=?, StartDate=? WHERE TaskID = ?";
-        jdbcTemplate.update(sql,task.getName(),task.getDescription(),task.getHourEstimate(),
-                task.isCompleted(),task.getDeadLine(),task.getStartDate(),task.getTaskID());
+        jdbcTemplate.update(sql, task.getName(), task.getDescription(), task.getHourEstimate(),
+                task.isCompleted(), task.getDeadLine(), task.getStartDate(), task.getTaskID());
         return task;
     }
 
     @Override
-    public boolean deleteTask(int taskID) throws  DataAccessException{
+    public boolean deleteTask(int taskID) throws DataAccessException {
         String sql = "DELETE FROM tasks where TaskID = ?";
-        int deletedRows = jdbcTemplate.update(sql,taskID);
-        if(deletedRows == 0){
-            return false;
-        }
-        return true;
+        int deletedRows = jdbcTemplate.update(sql, taskID);
+        return deletedRows != 0;
     }
 }
