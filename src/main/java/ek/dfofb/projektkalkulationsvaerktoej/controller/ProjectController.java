9c8d4df36a97692fd39c3e3b3a9caf1d381a169a
Patrick Schaufuss <pajo0001@stud.ek.dk>
2025-11-27 15:46:11 +0100

    Project User Story ADDED: Project (MODEL) Project (CONTROLLER) Project (REPOSITORY) Project (ROWMAPPER) Project (SERVICE) create.html list.html Unit + Mockito Tests for Project h2-test for Project

--- src/main/java/ek/dfofb/projektkalkulationsvaerktoej/controller/ProjectController.java
+++ src/main/java/ek/dfofb/projektkalkulationsvaerktoej/controller/ProjectController.java
@@ -1,8 +1,45 @@
 package ek.dfofb.projektkalkulationsvaerktoej.controller;
 
+import ek.dfofb.projektkalkulationsvaerktoej.model.Project;
+import ek.dfofb.projektkalkulationsvaerktoej.service.ProjectService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProjectController
 {
+    private final ProjectService projectService;
+
+    public ProjectController(ProjectService projectService)
+    {
+        this.projectService = projectService;
+    }
+
+    @GetMapping("/list")
+    public String listProjects(Model model)
+    {
+        model.addAttribute("projects", projectService.getAllProjects());
+        return "project/list";
+    }
+
+    @GetMapping("/create")
+    public String showCreateForm(Model model)
+    {
+        Project project = new Project();
+        project.setActive(true);
+        model.addAttribute("project", project);
+        return "project/create";
+    }
+
+    @PostMapping("/create")
+    public String handleCreateForm(@ModelAttribute("project") Project project)
+    {
+        projectService.createProject(project);
+        return "redirect:/project/list";
+    }
 }
