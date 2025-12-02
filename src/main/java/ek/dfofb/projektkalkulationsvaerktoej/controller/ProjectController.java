16086cc73c3423a8d954fd06569e64cb080300c2
Frede-72 <frjo0005@stud.kea.dk>
2025-12-02 09:46:44 +0100

    ADDED: project id gets saved in session Ability to create new tasks

--- src/main/java/ek/dfofb/projektkalkulationsvaerktoej/controller/ProjectController.java
+++ src/main/java/ek/dfofb/projektkalkulationsvaerktoej/controller/ProjectController.java
@@ -1,21 +1,23 @@
 package ek.dfofb.projektkalkulationsvaerktoej.controller;
 
 import ek.dfofb.projektkalkulationsvaerktoej.model.Project;
+import ek.dfofb.projektkalkulationsvaerktoej.model.Task;
 import ek.dfofb.projektkalkulationsvaerktoej.service.ProjectService;
+import ek.dfofb.projektkalkulationsvaerktoej.service.TaskService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("project")
 public class ProjectController {
     private final ProjectService projectService;
+    private final TaskService taskService;
 
-    public ProjectController(ProjectService projectService) {
+    public ProjectController(ProjectService projectService, TaskService taskService) {
         this.projectService = projectService;
+        this.taskService = taskService;
     }
 
     @GetMapping("/list")
@@ -36,5 +38,43 @@ public class ProjectController {
     public String handleCreateForm(@ModelAttribute Project project) {
         projectService.createProject(project);
         return "redirect:/project/list";
+    }
+
+    @PostMapping("/getID")
+    public String saveCurrentProjectID(String projectName, int projectID, HttpSession httpSession) {
+        httpSession.setAttribute(projectName, projectID);
+        return "redirect:/project/" + projectName;
+    }
+
+    @GetMapping("/{projectName}")
+    public String showProject(Model model, @PathVariable String projectName, HttpSession httpSession) {
+        if (httpSession.getAttribute(projectName) == null) {
+            return "redirect:/project/list";
+        }
+        int projectID = (int) httpSession.getAttribute(projectName);
+        Project project = projectService.getProjectByID(projectID);
+        model.addAttribute("project", project);
+        return "showProject";
+    }
+
+    @GetMapping("/{projectName}/create/task")
+    public String createTask(Model model, @PathVariable String projectName, HttpSession httpSession) {
+        if (httpSession.getAttribute(projectName) == null) {
+            return "redirect:/project/list";
+        }
+        Task task = new Task();
+        task.setProjectID((Integer) httpSession.getAttribute(projectName));
+        int projectID = (Integer) httpSession.getAttribute(projectName);
+        System.out.println(projectID);
+        model.addAttribute("task", task);
+        return "create-task-form";
+    }
+
+    @PostMapping("/create/task")
+    public String addTask(@ModelAttribute Task task) {
+        System.out.println(task.getProjectID());
+        taskService.addTask(task);
+        //Redirect til listen af tasks eller den nye task
+        return ("redirect:/project/list");
     }
 }
