ee53b8897119458a99f24a6257a778dc5c9fb4fc
Frede-72 <frjo0005@stud.kea.dk>
2025-12-05 11:26:51 +0100

    MOVED: task methods from ProjectController into TaskController

--- src/main/java/ek/dfofb/projektkalkulationsvaerktoej/controller/ProjectController.java
+++ src/main/java/ek/dfofb/projektkalkulationsvaerktoej/controller/ProjectController.java
@@ -57,16 +57,6 @@ public class ProjectController {
         return "redirect:/project/" + projectName;
     }
 
-    @PostMapping("/task/getID")
-    public String saveCurrentTaskID(String taskName, int taskID, HttpSession httpSession) {
-        String projectName = projectService.getProjectByID(taskService.getTaskByID(taskID).getProjectID()).getName();
-        httpSession.setAttribute("currentTask", taskID);
-        int parentID = taskService.getTaskByID(taskID).getParentID();
-        if (parentID == 0) {
-            return "redirect:/project/" + projectName + "/" + taskName;
-        }
-        return "redirect:/project/" + projectName + "/" + taskService.getTaskByID(parentID).getName() + "/" + taskName;
-    }
 
     @GetMapping("/{projectName}")
     public String showProject(Model model, @PathVariable String projectName, HttpSession httpSession) {
@@ -92,93 +82,6 @@ public class ProjectController {
         return "show-project";
     }
 
-    @GetMapping("/{projectName}/create/task")
-    public String createTask(Model model, @PathVariable String projectName, HttpSession httpSession) {
-        Account account = (Account) httpSession.getAttribute("account");
-        if (account == null) {
-            return "redirect:/account/login";
-        }
-        if (httpSession.getAttribute(projectName) == null) {
-            return "redirect:/project/list";
-        }
-        Task task = new Task();
-        task.setProjectID((Integer) httpSession.getAttribute(projectName));
-        model.addAttribute("task", task);
-        return "create-task-form";
-    }
-
-    @GetMapping("/{projectName}/{taskName}/create/task")
-    public String createSubTask(Model model, @PathVariable String projectName, @PathVariable String taskName, HttpSession httpSession) {
-        Account account = (Account) httpSession.getAttribute("account");
-        if (account == null) {
-            return "redirect:/account/login";
-        }
-        int taskID = (Integer) httpSession.getAttribute("currentTask");
-        String currentTask = taskService.getTaskByID(taskID).getName();
-        if (httpSession.getAttribute(projectName) == null || httpSession.getAttribute("currentTask") == null || !currentTask.equalsIgnoreCase(taskName)) {
-            return "redirect:/project/list";
-        }
-        Task task = new Task();
-        task.setProjectID((Integer) httpSession.getAttribute(projectName));
-        task.setParentID((Integer) httpSession.getAttribute("currentTask"));
-        model.addAttribute("task", task);
-        return "create-sub-task-form";
-    }
-
-    @PostMapping("/create/task")
-    public String addTask(@ModelAttribute Task task) {
-        taskService.addTask(task);
-        if (task.getParentID() != 0) {
-            String projectName = projectService.getProjectByID(task.getProjectID()).getName();
-            String parentName = taskService.getTaskByID(task.getParentID()).getName();
-            return ("redirect:/project/" + projectName + "/" + parentName);
-        }
-        String projectName = projectService.getProjectByID(task.getProjectID()).getName();
-        return ("redirect:/project/" + projectName);
-    }
-
-    @GetMapping("/{projectName}/{taskName}")
-    public String showTask(Model model, @PathVariable String projectName, @PathVariable String taskName, HttpSession httpSession) {
-        Account account = (Account) httpSession.getAttribute("account");
-        if (account == null) {
-            return "redirect:/account/login";
-        }
-        int taskID = (Integer) httpSession.getAttribute("currentTask");
-        String name = taskService.getTaskByID(taskID).getName();
-        if (httpSession.getAttribute(projectName) == null || httpSession.getAttribute("currentTask") == null || !name.equalsIgnoreCase(taskName)) {
-            return "redirect:/project/list";
-        }
-        model.addAttribute("task", taskService.getTaskByID(taskID));
-        model.addAttribute("tasks", taskService.getAllSubTasks(taskID));
-        model.addAttribute("hourEstimate", taskService.hoursLeftOnTask(taskID));
-        model.addAttribute("projectName", projectName);
-        return "show-task";
-    }
-
-    @GetMapping("/{projectName}/{taskName}/{subTaskName}")
-    public String showSubTask(Model model, @PathVariable String projectName, @PathVariable String taskName, @PathVariable(required = false) String subTaskName, HttpSession httpSession) {
-        Account account = (Account) httpSession.getAttribute("account");
-        if (account == null) {
-            return "redirect:/account/login";
-        }
-        int taskID = (Integer) httpSession.getAttribute("currentTask");
-        int mainTaskID = taskService.getTaskByID(taskID).getParentID();
-        String currentTask = taskService.getTaskByID(taskID).getName();
-        String mainTaskName = taskService.getTaskByID(mainTaskID).getName();
-        if (httpSession.getAttribute(projectName) == null || httpSession.getAttribute("currentTask") == null
-                || !currentTask.equalsIgnoreCase(subTaskName) || !mainTaskName.equalsIgnoreCase(taskName)) {
-            return "redirect:/project/list";
-        }
-        model.addAttribute("task", taskService.getTaskByID(taskID));
-        model.addAttribute("tasks", taskService.getAllSubTasks(taskID));
-        model.addAttribute("projectName", projectName);
-        model.addAttribute("subTaskName", currentTask);
-        model.addAttribute("mainTaskID", mainTaskID);
-        model.addAttribute("mainTask", mainTaskName);
-        model.addAttribute("hourEstimate", taskService.hoursLeftOnTask(taskID));
-        return "show-task";
-    }
-
     @GetMapping("/{projectName}/edit")
     public String editProject(Model model, @PathVariable String projectName, HttpSession httpSession) {
         Account account = (Account) httpSession.getAttribute("account");
@@ -194,19 +97,6 @@ public class ProjectController {
         return "edit-project-form";
     }
 
-    @GetMapping("/{projectName}/{taskName}/edit")
-    public String editTask(Model model, @PathVariable String projectName, @PathVariable String taskName, HttpSession httpSession) {
-        Account account = (Account) httpSession.getAttribute("account");
-        if (account == null) {
-            return "redirect:/account/login";
-        }
-        if (httpSession.getAttribute(projectName) == null || httpSession.getAttribute("currentTask") == null) {
-            return "redirect:/project/list";
-        }
-        Task task = taskService.getTaskByID((Integer) httpSession.getAttribute("currentTask"));
-        model.addAttribute("task", task);
-        return "edit-task-form";
-    }
 
     @PostMapping("/edit")
     public String updateProject(@ModelAttribute Project project, HttpSession httpSession) {
@@ -214,12 +104,4 @@ public class ProjectController {
         String projectName = projectService.getProjectByID(project.getProjectID()).getName();
         return saveCurrentProjectID(projectName, project.getProjectID(), httpSession);
     }
-
-    @PostMapping("/task/edit")
-    public String updateTask(@ModelAttribute Task task, HttpSession httpSession) {
-        taskService.updateTask(task);
-        String taskName = taskService.getTaskByID(task.getTaskID()).getName();
-        return saveCurrentTaskID(taskName, task.getTaskID(), httpSession);
-    }
-
 }
---
+++ src/main/java/ek/dfofb/projektkalkulationsvaerktoej/controller/TaskController.java
@@ -0,0 +1,145 @@
+package ek.dfofb.projektkalkulationsvaerktoej.controller;
+
+import ek.dfofb.projektkalkulationsvaerktoej.model.Account;
+import ek.dfofb.projektkalkulationsvaerktoej.model.Task;
+import ek.dfofb.projektkalkulationsvaerktoej.service.ProjectService;
+import ek.dfofb.projektkalkulationsvaerktoej.service.TaskService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("project")
+public class TaskController {
+
+    private ProjectService projectService;
+    private TaskService taskService;
+
+    public TaskController(ProjectService projectService, TaskService taskService) {
+        this.projectService = projectService;
+        this.taskService = taskService;
+    }
+
+    @PostMapping("/task/getID")
+    public String saveCurrentTaskID(String taskName, int taskID, HttpSession httpSession) {
+        String projectName = projectService.getProjectByID(taskService.getTaskByID(taskID).getProjectID()).getName();
+        httpSession.setAttribute("currentTask", taskID);
+        int parentID = taskService.getTaskByID(taskID).getParentID();
+        if (parentID == 0) {
+            return "redirect:/project/" + projectName + "/" + taskName;
+        }
+        return "redirect:/project/" + projectName + "/" + taskService.getTaskByID(parentID).getName() + "/" + taskName;
+    }
+
+    @GetMapping("/{projectName}/create/task")
+    public String createTask(Model model, @PathVariable String projectName, HttpSession httpSession) {
+        Account account = (Account) httpSession.getAttribute("account");
+        if (account == null) {
+            return "redirect:/account/login";
+        }
+        if (httpSession.getAttribute(projectName) == null) {
+            return "redirect:/project/list";
+        }
+        Task task = new Task();
+        task.setProjectID((Integer) httpSession.getAttribute(projectName));
+        model.addAttribute("task", task);
+        return "create-task-form";
+    }
+
+    @GetMapping("/{projectName}/{taskName}/create/task")
+    public String createSubTask(Model model, @PathVariable String projectName, @PathVariable String taskName, HttpSession httpSession) {
+        Account account = (Account) httpSession.getAttribute("account");
+        if (account == null) {
+            return "redirect:/account/login";
+        }
+        int taskID = (Integer) httpSession.getAttribute("currentTask");
+        String currentTask = taskService.getTaskByID(taskID).getName();
+        if (httpSession.getAttribute(projectName) == null || httpSession.getAttribute("currentTask") == null || !currentTask.equalsIgnoreCase(taskName)) {
+            return "redirect:/project/list";
+        }
+        Task task = new Task();
+        task.setProjectID((Integer) httpSession.getAttribute(projectName));
+        task.setParentID((Integer) httpSession.getAttribute("currentTask"));
+        model.addAttribute("task", task);
+        return "create-sub-task-form";
+    }
+
+    @PostMapping("/create/task")
+    public String addTask(@ModelAttribute Task task) {
+        taskService.addTask(task);
+        if (task.getParentID() != 0) {
+            String projectName = projectService.getProjectByID(task.getProjectID()).getName();
+            String parentName = taskService.getTaskByID(task.getParentID()).getName();
+            return ("redirect:/project/" + projectName + "/" + parentName);
+        }
+        String projectName = projectService.getProjectByID(task.getProjectID()).getName();
+        return ("redirect:/project/" + projectName);
+    }
+
+    @GetMapping("/{projectName}/{taskName}")
+    public String showTask(Model model, @PathVariable String projectName, @PathVariable String taskName, HttpSession httpSession) {
+        Account account = (Account) httpSession.getAttribute("account");
+        if (account == null) {
+            return "redirect:/account/login";
+        }
+        int taskID = (Integer) httpSession.getAttribute("currentTask");
+        String name = taskService.getTaskByID(taskID).getName();
+        if (httpSession.getAttribute(projectName) == null || httpSession.getAttribute("currentTask") == null || !name.equalsIgnoreCase(taskName)) {
+            return "redirect:/project/list";
+        }
+        model.addAttribute("task", taskService.getTaskByID(taskID));
+        model.addAttribute("tasks", taskService.getAllSubTasks(taskID));
+        model.addAttribute("hourEstimate", taskService.hoursLeftOnTask(taskID));
+        model.addAttribute("projectName", projectName);
+        return "show-task";
+    }
+
+    @GetMapping("/{projectName}/{taskName}/{subTaskName}")
+    public String showSubTask(Model model, @PathVariable String projectName, @PathVariable String taskName, @PathVariable(required = false) String subTaskName, HttpSession httpSession) {
+        Account account = (Account) httpSession.getAttribute("account");
+        if (account == null) {
+            return "redirect:/account/login";
+        }
+        int taskID = (Integer) httpSession.getAttribute("currentTask");
+        int mainTaskID = taskService.getTaskByID(taskID).getParentID();
+        String currentTask = taskService.getTaskByID(taskID).getName();
+        String mainTaskName = taskService.getTaskByID(mainTaskID).getName();
+        if (httpSession.getAttribute(projectName) == null || httpSession.getAttribute("currentTask") == null
+                || !currentTask.equalsIgnoreCase(subTaskName) || !mainTaskName.equalsIgnoreCase(taskName)) {
+            return "redirect:/project/list";
+        }
+        model.addAttribute("task", taskService.getTaskByID(taskID));
+        model.addAttribute("tasks", taskService.getAllSubTasks(taskID));
+        model.addAttribute("projectName", projectName);
+        model.addAttribute("subTaskName", currentTask);
+        model.addAttribute("mainTaskID", mainTaskID);
+        model.addAttribute("mainTask", mainTaskName);
+        model.addAttribute("hourEstimate", taskService.hoursLeftOnTask(taskID));
+        return "show-task";
+    }
+
+
+    @GetMapping("/{projectName}/{taskName}/edit")
+    public String editTask(Model model, @PathVariable String projectName, @PathVariable String taskName, HttpSession httpSession) {
+        Account account = (Account) httpSession.getAttribute("account");
+        if (account == null) {
+            return "redirect:/account/login";
+        }
+        if (httpSession.getAttribute(projectName) == null || httpSession.getAttribute("currentTask") == null) {
+            return "redirect:/project/list";
+        }
+        Task task = taskService.getTaskByID((Integer) httpSession.getAttribute("currentTask"));
+        model.addAttribute("task", task);
+        return "edit-task-form";
+    }
+
+
+    @PostMapping("/task/edit")
+    public String updateTask(@ModelAttribute Task task, HttpSession httpSession) {
+        taskService.updateTask(task);
+        String taskName = taskService.getTaskByID(task.getTaskID()).getName();
+        return saveCurrentTaskID(taskName, task.getTaskID(), httpSession);
+    }
+
+}
