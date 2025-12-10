3d392a2da5285980c340a1892f90db79834a64e5
Frede-72 <frjo0005@stud.kea.dk>
2025-12-10 10:28:20 +0100

    ADDED: dd-MM-yyyy format

--- src/main/java/ek/dfofb/projektkalkulationsvaerktoej/controller/TaskController.java
+++ src/main/java/ek/dfofb/projektkalkulationsvaerktoej/controller/TaskController.java
@@ -102,7 +102,7 @@ public class TaskController {
         if (account == null) {
             return "redirect:/account/login";
         }
-        if (!authorizationService.hasPermission(account.getRoleID(), Permission.ADD_PROJECTS) || task.getIsCompleted() == true) {
+        if (!authorizationService.hasPermission(account.getRoleID(), Permission.ADD_PROJECTS) || task.getIsCompleted()) {
             return "redirect:/project";
         }
         taskService.addTask(task);
